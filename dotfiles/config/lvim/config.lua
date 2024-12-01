70272d173b9c540a75506f7408aab07cfd754da0
baolhq <baolhq280@gmail.com>
2024-12-01 22:35:12 +0700

    update dotfiles

--- dotfiles/config/lvim/config.lua
+++ dotfiles/config/lvim/config.lua
@@ -1,9 +1,3 @@
--- Read the docs: https://www.lunarvim.org/docs/configuration
--- Example configs: https://github.com/LunarVim/starter.lvim
--- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
--- Forum: https://www.reddit.com/r/lunarvim/
--- Discord: https://discord.com/invite/Xb9B4Ny
-
 -- Plugins
 lvim.plugins = {
   { "rose-pine/neovim", name = "rose-pine" }
@@ -14,3 +8,11 @@ lvim.colorscheme = "rose-pine"
 
 -- Keybindings
 lvim.keys.insert_mode["jk"] = "<esc>"
+
+-- WhichKey mappings
+lvim.builtin.which_key.mappings["t"] = {
+  name = "+Terminal",
+  f = { "<cmd>ToggleTerm<cr>", "Floating terminal" },
+  v = { "<cmd>2ToggleTerm size=30 direction=vertical<cr>", "Split vertical" },
+  h = { "<cmd>2ToggleTerm size=10 direction=horizontal<cr>", "Split horizontal" },
+}
